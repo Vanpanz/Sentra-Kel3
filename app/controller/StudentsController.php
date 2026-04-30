02ca03635e3b08dadadca3624fc6a9e42b640855
Kaptento <louisgracia456@gmail.com>
2026-04-30 13:52:19 +0700

    wip:implement code studentscotroller

--- app/controller/StudentsController.php
+++ app/controller/StudentsController.php
@@ -1,16 +1,14 @@
 <?php
 namespace App\Controllers;
 
-class StudentsController { // Pastikan namanya StudentsController
+class StudentsController {
+
     public function index() {
         require_once '../app/views/auth/login.php';
     }
-}
 
     public function show() {
         require_once '../app/views/event/detailevent.php';
     }
 
-
-
-?>
+}
